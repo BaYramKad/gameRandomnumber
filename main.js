7096c5e82b2d23bfef9ebe02b75b1f1714ab7d90
Bayram Emiraliev <bayram.kad@gmail.com>
2020-10-22 22:53:09 +0300

    Игра - Угадай число(Сложность - низкая) Тема - Замыкание

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
+"use strict";
 
-function randomGame(){
+let rDom = Math.floor(100 * Math.random()) + 1;
+function randomGame(rDom){
     let input = prompt("Введите число от 1 до 100");
-    let rDom = 45;
         function dom(input){
             if (input === null){
                 alert("Игра окончена"); 
             }
-            
-            console.dir(randomGame);
             if(isNaN(input) || input.trim() === ""){
                 alert("Введите число!");
             }else if(input == rDom){
-                alert("Поздравляем!!! Вы угадали")
+                alert("Поздравляем!!! Вы угадали");
+                location.loader();
             } else if(input > rDom){
                 alert("Загаданное число меньше");
             } else if(input < rDom){
                 alert("Загаданное число больше");
             } 
-            return randomGame(); 
+            randomGame(rDom); 
         }
-    return dom(input);
+    dom(input);
 }
-randomGame();
+randomGame(rDom);
